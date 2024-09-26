cca059f3af1de3f0682999ead22926705ff903d7
daiki01240 <domo01240@gmail.com>
2024-09-26 13:48:22 -0700

    quesiton 88

--- top-150-interview/88-merged-sorted-array.ts
+++ top-150-interview/88-merged-sorted-array.ts
@@ -2,9 +2,11 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number) {
-  console.log(nums1.splice(m, n, ...nums2));
-  //   return nums1.sort((a, b) => a - b);
+  nums1.splice(m, num1.length - m, ...nums2);
+  console.log(num1);
+  return nums1.sort((a, b) => a - b);
 }
+
 const num1 = [1, 2, 3, 0, 0, 0];
 const m = 3;
 const num2 = [2, 5, 6];
