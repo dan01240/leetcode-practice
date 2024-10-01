c6e605677da00c45e004848f563b7e90e0a6af6c
daiki01240 <domo01240@gmail.com>
2024-10-01 15:13:03 -0700

    commit

--- top-150-interview/88-merged-sorted-array.ts
+++ top-150-interview/88-merged-sorted-array.ts
@@ -6,13 +6,9 @@
 // }
 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  // nums1とnums2の最後の要素を指すポインタ
   let p1 = m - 1;
   let p2 = n - 1;
-  // nums1の末尾から要素を埋めていくポインタ
   let p = m + n - 1;
-
-  // nums1とnums2の要素を比較し、nums1の末尾から埋めていく
   while (p1 >= 0 && p2 >= 0) {
     if (nums1[p1] > nums2[p2]) {
       nums1[p] = nums1[p1];
@@ -23,10 +19,8 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     }
     p--;
   }
-
-  // nums2に要素が残っている場合、それをnums1の先頭にコピー
   while (p2 >= 0) {
-    nums1[p] = nums2[p2];
+    num1[p] = num2[p2];
     p2--;
     p--;
   }
